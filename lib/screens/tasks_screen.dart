d9d38831e2f585223971d992374e4a70d87b17d0
aasgh-dev <aasgh.devolper@gmail.com>
2025-09-18 16:54:54 +0300

    Add files via upload

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:todaydo_app/models/task_data.dart';
+import 'package:todaydo_app/screens/add_task_screen.dart';
+import 'package:todaydo_app/widgets/tasks_list.dart';
+import 'package:provider/provider.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
+                child: AddTaskScreen((newTaskTitle) {}),
+              ),
+            ),
+          );
+        },
+        backgroundColor: Colors.black,
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      backgroundColor: const Color.fromARGB(255, 65, 33, 243),
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 20,
+          right: 20,
+          bottom: 80,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.playlist_add_check, size: 40, color: Colors.white),
+                SizedBox(width: 20),
+                Text(
+                  'ToDayDo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '${Provider.of<TaskData>(context).tasks.length} Tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: TasksList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
